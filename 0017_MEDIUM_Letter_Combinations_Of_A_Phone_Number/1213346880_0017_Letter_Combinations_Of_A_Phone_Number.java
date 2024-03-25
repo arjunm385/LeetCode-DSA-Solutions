ea6b624b880a65eb247dbf4cbc650c91b6bc18fb
Arjun <86074282+arjunm385@users.noreply.github.com>
2024-03-25 15:15:45 +0530

    Time: 6 ms (12.26%), Space: 41.96 MB (66.75%) - LeetCodeSync

---
+++ 0017_MEDIUM_Letter_Combinations_Of_A_Phone_Number/1213346880_0017_Letter_Combinations_Of_A_Phone_Number.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+
+        List<String> list = new ArrayList<>();
+
+        if(digits.isBlank()) {
+            return list;
+        }
+        
+        String[] arr = {
+                "",
+                "",
+                "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
+
+        getWords(digits, arr, 0, "", list);
+
+        return list;
+        
+    }
+
+    static void getWords(String input, String[] arr, int index, String str, List<String> list) {
+        // Base Condition
+        if(index == input.length()) {
+            list.add(str);
+            return;
+        }
+
+        int pos = Integer.parseInt(("" + input.charAt(index)));
+
+        for(int i=0; i < arr[pos].length(); i++) {
+            getWords(input, arr, index+1, str + arr[pos].charAt(i), list);
+        }
+    }
+}
