bb8d3ecb3ba579713c67cd0c71719cf10f1187a0
Arjun <86074282+arjunm385@users.noreply.github.com>
2025-04-24 19:57:45 +0530

    Time: 6 ms (5.91%), Space: 46.2 MB (94.41%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        int count = 0;
+        int lastPrinted = Integer.MIN_VALUE;
+
+        for(int i=0; i<nums.length; i++) {
+            if(lastPrinted != nums[i]) {
+                lastPrinted = nums[i];
+                count=1;
+            }
+            else if(lastPrinted == nums[i] && count == 1) {
+                count++;
+            }
+            else{
+                nums[i] = Integer.MIN_VALUE;
+            }
+        }
+
+        int p=0;
+        count = 0;
+
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] != Integer.MIN_VALUE) {
+                swap(nums, i, p++);
+                count++;
+            }
+        }
+
+
+        System.out.println(Arrays.toString(nums));
+
+        return count;
+        
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
