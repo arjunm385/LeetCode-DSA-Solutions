232c981b75fb216c3e895799a5f98021e83b205a
Arjun <86074282+arjunm385@users.noreply.github.com>
2025-05-03 10:52:04 +0530

    Time: 48 ms (19.12%), Space: 45.4 MB (10.65%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        if(s1.length() > s2.length()) {
+            return false;
+        }
+
+        Map<Character,Integer> map= new HashMap<>();
+
+        int k=s1.length();
+
+        for(Character c: s1.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+        for(int i=0;i<k;i++){
+            if(map.containsKey(s2.charAt(i))){
+                map.put(s2.charAt(i),map.get(s2.charAt(i)) - 1);
+            }
+        }
+
+        if(check(map)) {
+            return true;
+        }
+
+        for(int i=k; i<s2.length();i++){
+            
+            if(map.containsKey(s2.charAt(i))) {
+                map.put(s2.charAt(i), map.get(s2.charAt(i)) - 1);
+            }
+
+            if(map.containsKey(s2.charAt(i-k))) {
+                map.put(s2.charAt(i-k), map.get(s2.charAt(i-k)) + 1);
+            }
+
+            if(check(map))
+                return true;
+
+        }
+
+        return false;
+        
+    }
+
+    public boolean check(Map<Character,Integer> map) {
+        return map.values().stream().filter(v-> v>0).count()==0;
+    }
+}
